fd547a4f5eb77baad22082e364d7ec37b4f9901b
ВТБ <nikita.shevnin2016@yandex.ru>
2024-04-07 13:41:32 +0300

    solution sem1

---
+++ src/main/java/Sem1/ClassWork/task2/Market.java
@@ -0,0 +1,14 @@
+package Sem1.ClassWork.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Market<T extends Food> {
+
+    public List<Food> balanceFoodstuffs(List<T> foodstuffs) {
+        // реализация балансировки товаров в магазине
+        List<Food> balancedList = new ArrayList<>();
+        // ваша логика балансировки
+        return balancedList;
+    }
+}
